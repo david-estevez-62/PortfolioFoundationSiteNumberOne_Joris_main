d4401f579a5bba5503ba3f78d888601ba936eb4f
David Estevez <autoreplybot@aol.com>
2023-05-05 02:46:18 +0000

    Portfolio Foundation Site Number One - Joris - Main

--- js/main.js
+++ js/main.js
@@ -2,81 +2,122 @@ $(function(){
 
 
 
-	$(".sect1").on('mouseover', function(){
+	$(".sect1_visible_part").on('mouseover', function(){
 		$(".table1").css({top:0});
 		$(".block1").css({top:20});
-		$(this).find("div hr:nth-child(1)").css({top:21});
+		$(this).parent().find("div hr:nth-child(1)").css({top:21});
+    $(this).css({top:0, height:78});
 	})
-	$(".sect1").on('mouseout', function(){
-		$(".table1").css({top:20});
-		$(".block1").css({top:0});
-		$(this).find("div hr:nth-child(1)").css({top:41});
-	})
+	$(".sect1_visible_part").on('mouseout', function(){
+		$(".table1").css({top:16});
+		$(".block1").css({top:4});
+		$(this).parent().find("div hr:nth-child(1)").css({top:37});
+	  $(this).css({top:16, height:44});
+  })
 
 
 
-	$(".sect2").on('mouseover', function(){
+	$(".sect2_visible_part").on('mouseover', function(){
 		$(".table2").css({top:0});
 		$(".block2").css({top:20});
-		$(this).find("div hr:nth-child(1)").css({top:21});
+		$(this).parent().find("div hr:nth-child(1)").css({top:21});
+    $(this).css({top:0, height:78});
 	})
-	$(".sect2").on('mouseout', function(){
-		$(".table2").css({top:20});
-		$(".block2").css({top:0});
-		$(this).find("div hr:nth-child(1)").css({top:41});
+	$(".sect2_visible_part").on('mouseout', function(){
+		$(".table2").css({top:16});
+		$(".block2").css({top:4});
+		$(this).parent().find("div hr:nth-child(1)").css({top:37});
+    $(this).css({top:16, height:44});
 	})
 
 
 
-	$(".sect3").on('mouseover', function(){
+	$(".sect3_visible_part").on('mouseover', function(){
 		$(".table3").css({top:0});
 		$(".block3").css({top:20});
-		$(this).find("div hr:nth-child(1)").css({top:21});
+		$(this).parent().find("div hr:nth-child(1)").css({top:21});
+    $(this).css({top:0, height:78});
 	})
-	$(".sect3").on('mouseout', function(){
-		$(".table3").css({top:20});
-		$(".block3").css({top:0});
-		$(this).find("div hr:nth-child(1)").css({top:41});
+	$(".sect3_visible_part").on('mouseout', function(){
+		$(".table3").css({top:16});
+		$(".block3").css({top:4});
+		$(this).parent().find("div hr:nth-child(1)").css({top:37});
+    $(this).css({top:16, height:44});
 	})
 
 
 
-	$(".sect4").on('mouseover', function(){
+	$(".sect4_visible_part").on('mouseover', function(){
 		$(".table4").css({top:0});
 		$(".block4").css({top:20});
-		$(this).find("div hr:nth-child(1)").css({top:21});
+		$(this).parent().find("div hr:nth-child(1)").css({top:21});
+    $(this).css({top:0, height:78});
 	})
-	$(".sect4").on('mouseout', function(){
-		$(".table4").css({top:20});
-		$(".block4").css({top:0});
-		$(this).find("div hr:nth-child(1)").css({top:41});
+	$(".sect4_visible_part").on('mouseout', function(){
+		$(".table4").css({top:16});
+		$(".block4").css({top:4});
+		$(this).parent().find("div hr:nth-child(1)").css({top:37});
+    $(this).css({top:16, height:44});
 	})
 
 
 
-	$(".sect5").on('mouseover', function(){
+	$(".sect5_visible_part").on('mouseover', function(){
 		$(".table5").css({top:0});
 		$(".block5").css({top:20});
-		$(this).find("div hr:nth-child(1)").css({top:21});
+		$(this).parent().find("div hr:nth-child(1)").css({top:21});
+    $(this).css({top:0, height:78});
 	})
-	$(".sect5").on('mouseout', function(){
-		$(".table5").css({top:20});
-		$(".block5").css({top:0});
-		$(this).find("div hr:nth-child(1)").css({top:41});
+	$(".sect5_visible_part").on('mouseout', function(){
+		$(".table5").css({top:16});
+		$(".block5").css({top:4});
+		$(this).parent().find("div hr:nth-child(1)").css({top:37});
+    $(this).css({top:16, height:44});
 	})
 
 
 
-	$(".sect6").on('mouseover', function(){
+	$(".sect6_visible_part").on('mouseover', function(){
 		$(".table6").css({top:0});
 		$(".block6").css({top:20});
-		$(this).find("div hr:nth-child(1)").css({top:21});
+		$(this).parent().find("div hr:nth-child(1)").css({top:21});
+    $(this).css({top:0, height:78});
 	})
-	$(".sect6").on('mouseout', function(){
-		$(".table6").css({top:20});
-		$(".block6").css({top:0});
-		$(this).find("div hr:nth-child(1)").css({top:41});
+	$(".sect6_visible_part").on('mouseout', function(){
+		$(".table6").css({top:16});
+		$(".block6").css({top:4});
+		$(this).parent().find("div hr:nth-child(1)").css({top:37});
+    $(this).css({top:16, height:44});
 	})
+
+
+
+
+
+
+$(".sect1_visible_part").on('click', function(){
+  window.location.href = "http://erichr.me";
+})
+
+$(".sect2_visible_part").on('click', function(){
+  window.location.href = "http://portfoliobaselinesitenum2.herokuapp.com";
+})
+
+$(".sect3_visible_part").on('click', function(){
+  window.location.href = "http://portfoliobaselinesitenum3.herokuapp.com";
+})
+
+$(".sect4_visible_part").on('click', function(){
+  window.location.href = "#!";
+})
+
+$(".sect5_visible_part").on('click', function(){
+  window.location.href = "#!";
+})
+
+$(".sect6_visible_part").on('click', function(){
+  window.location.href = "#!";
+})
 
 
 
